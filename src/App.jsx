00d4b1b34e0a070fd283b36d15f308c11e14c743
Krishna Nand Patel <imkrishna8737@gmail.com>
2023-02-27 18:29:30 +0530

    Suneditor done
    Signed-off-by: Krishna Nand Patel <imkrishna8737@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Editor from './SunEditor/Editor'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Editor></Editor>
+    </div>
+  )
+}
+
+export default App
